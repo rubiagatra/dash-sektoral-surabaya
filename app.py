dc5e4993ea28a179468729f7d015a25affa06412
Doni Rubiagatra <doni.rubiagatra@kumparan.com>
2018-06-04 05:46:16 +0700

    adding curah hujan

--- app.py
+++ app.py
@@ -34,6 +34,34 @@ app.layout = html.Div([
     html.Div(id='graphs')   
 ], className='container')
 
+def curah_hujan():
+    perak = df["Curah Hujan (Perak)"]
+    juanda = df["Curah Hujan (Juanda)"] 
+    data1 = go.Scatter(x=df.index, y=perak, name="Stasiun Perak II", mode="lines+markers")
+    data2 = go.Scatter(x=df.index, y=juanda, name="Stasiun Juanda", mode="lines+markers")
+    data = [data1, data2]
+    layout = dict(title = 'Rata Rata Curah Hujan di Surabaya Tahun 2016',
+                xaxis = dict(title = 'Bulan'),
+                yaxis = dict(title = 'Curah Hujan (mm)'),
+                  )
+    fig = dict(data=data, layout=layout)
+    return dcc.Graph(
+        id='curah',
+        figure=fig,
+    )
+
+@app.callback(
+    Output('graphs', 'children'),
+    [Input('data-input', 'value')],
+)
+def update_graph(data):
+    graphs = []
+    for i, data in enumerate(data):
+        if data == 'curah-hujan':
+            graphs.append(curah_hujan())
+    
+    return graphs
+
 external_css = ["https://fonts.googleapis.com/css?family=Product+Sans:400,400i,700,700i",
                 "https://cdn.rawgit.com/plotly/dash-app-stylesheets/2cc54b8c03f4126569a3440aae611bbef1d7a5dd/stylesheet.css"]
 
